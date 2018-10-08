6d14c7112767a5214d3d2e87140c19dc03281652
WillGreen98 <will.green98@hotmail.com>
2018-10-08 20:17:11 +0100

    Week 1

---
+++ Week 1/src/delay/Delay.java
@@ -0,0 +1,15 @@
+package delay;
+
+public class Delay {
+    public static void main(String args[]) throws Exception {
+        System.out.println("starting delay code");
+
+        long start = System.currentTimeMillis();
+        int delay = (int) (1000 * Math.random());
+        Thread.sleep(delay);
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("delay was " + (end - start) + " milliseconds.") ;
+    }
+}
---
+++ Week 1/src/helloWorldApplication/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloWorldApplication;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        String firstName = args[0];
+        String secondName = args[1];
+
+        System.out.println("Hello " + firstName + " " + secondName + "!");
+    }
+}
